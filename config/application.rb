7cda04ce21e146ad7d473d65a04b89460ae57c9e
Tymon Tobolski <i@teamon.eu>
2013-07-10 19:56:05 +0200

    fix: Adam niggeloading

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,10 @@ module GhcrWeb
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.autoload_paths += %W(
+      #{config.root}/lib
+    )
+
     # Disable the asset pipeline.
     config.assets.enabled = false
 
