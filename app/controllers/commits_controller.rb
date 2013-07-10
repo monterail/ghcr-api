19178be0a92d5aeeffd00696eb2252c31b049ca9
Tymon Tobolski <i@teamon.eu>
2013-07-10 17:48:56 +0200

    Commits index filters

--- app/controllers/commits_controller.rb
+++ app/controllers/commits_controller.rb
@@ -1,6 +1,23 @@
 class CommitsController < ApplicationController
   def index
-    commits = repo.commits.all
+    commits = repo.commits
+
+    # if value starts with "!" (bang) - negation
+    #
+    # params[:author] == "teamon"   -> where author = 'teamon'
+    # params[:author] == "!teamon"  -> where author <> 'teamon'
+    [:sha, :author, :status].each do |key|
+      unless params[key].blank?
+        params[key].to_s.split(",").each do |e|
+          if e =~ /^!(.+)$/
+            commits = commits.where("#{key} <> ?", $1)
+          else
+            commits = commits.where(key => e)
+          end
+        end
+      end
+    end
+
     render :json => commits.to_json
   end
 
