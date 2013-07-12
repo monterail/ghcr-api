ad900d37d8e682cbc46cea609dfab48664c59bce
Adam Stankiewicz <sheerun@sher.pl>
2013-07-12 18:01:27 +0200

    Modify github controller test

--- spec/controllers/github_controller_spec.rb
+++ spec/controllers/github_controller_spec.rb
@@ -2,11 +2,13 @@ require 'spec_helper'
 
 describe GithubController do
   describe 'POST payload' do
-    it 'should receive sample payload' do
+    it 'should add new commits' do
       payload = File.read(
         File.join(File.dirname(__FILE__), '../fixtures/test_payload.json'))
 
-      post :payload, payload: payload, format: :json
+      expect {
+        post :payload, payload: payload, format: :json
+      }.to change { Commit.count }.by(3)
       
       expect(response.status).to eq(200)
     end
