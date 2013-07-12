e1b3ff53048cb24f6e3775e3982b21dfef56c3d8
Adam Stankiewicz <sheerun@sher.pl>
2013-07-12 15:01:07 +0200

    Add github payload url

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ GhcrWeb::Application.routes.draw do
   scope "/api/v1/" do
     get '/authorize/callback', to: 'authorizations#create', default: { provider: 'github' }
 
+    post '/github', :to => "github#payload"
+
     scope ":owner/:repo" do
       resources :commits
       resource :reminder, only: [:show, :create, :update, :destroy]
