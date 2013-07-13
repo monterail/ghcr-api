80b8315995c6d02b0c91c291888956453c39c5e2
Ostrzy <uslugi@ostrzy.net>
2013-07-13 21:23:25 +0200

    Drop before_action in favor of lazy loading
    accepts 5cd515d68c9d25be43dfcf345ae3791a592d9f20

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -1,22 +1,19 @@
 class RemindersController < ApplicationController
-  before_action :find_repo
-  before_action :find_reminder
-
   def show
-    render json: { active: !!@reminder.try(:active) }
+    render json: { active: !!reminder.try(:active) }
   end
 
   def create
-    if @reminder
-      @reminder.activate
+    if reminder
+      reminder.activate
     else
-      current_user.reminders.create(repository: @repository)
+      current_user.reminders.create(repository: repository)
     end
     render nothing: true
   end
 
   def update
-    if @reminder && @reminder.update_attributes(params[:reminder])
+    if reminder && reminder.update_attributes(params[:reminder])
       render status: 200
     else
       render status: 422
@@ -24,8 +21,8 @@ class RemindersController < ApplicationController
   end
 
   def destroy
-    if @reminder
-      @reminder.deactivate
+    if reminder
+      reminder.deactivate
     else
       not_found
     end
@@ -33,12 +30,12 @@ class RemindersController < ApplicationController
   end
 
   private
-    def find_repo
-      @repository = Repository.where(owner: params[:owner], name: params[:repo]).first || not_found
+    def repository
+      @repository ||= Repository.where(owner: params[:owner], name: params[:repo]).first || not_found
     end
 
-    def find_reminder
-      @reminder = Reminder.find_by_user_and_repository(current_user, @repository)
+    def reminder
+      @reminder ||= Reminder.find_by_user_and_repository(current_user, repository)
     end
 
     def reminder_params
