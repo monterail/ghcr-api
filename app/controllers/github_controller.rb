586d48b946a4773f83079dc858f32c253a209ff0
Adam Stankiewicz <sheerun@sher.pl>
2013-07-20 22:17:29 +0200

    fix: Typo

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -5,10 +5,11 @@ class GithubController < ApplicationController
     if repo.present?
       pending = repo.commits.query(author: "!#{current_user.username}", status: "pending")
       rejected = repo.commits.query(author: current_user.username, status: "rejected")
+
       render json: {
         username: current_user.username,
         rejected: rejected.map(&:response_hash),
-        pending: rejected.map(&:response_hash)
+        pending: pending.map(&:response_hash)
       }
     else
       render json: {
