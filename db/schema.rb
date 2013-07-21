044cbebccd0f938582f521d0ed962377854d1176
Adam Stankiewicz <sheerun@sher.pl>
2013-07-21 02:37:24 +0200

    Rename access_token to github_access_token

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130721000740) do
+ActiveRecord::Schema.define(version: 20130721003605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,7 +77,7 @@ ActiveRecord::Schema.define(version: 20130721000740) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "access_token"
+    t.string   "github_access_token"
   end
 
 end
