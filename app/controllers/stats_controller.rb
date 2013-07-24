12dab3250e004efdbd9159983b6bb7c9c13094fc
Ostrzy <uslugi@ostrzy.net>
2013-07-25 00:25:51 +0200

    Stats fixed and improved

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,24 +1,21 @@
 class StatsController < ApplicationController
   def show
     shas = params[:repos].to_s.split(',')
-    repos = Repository.where(:access_token => shas).pluck(&:id)
-    chain_start = Commit.where(:repository_id => repos.map(&:id))
+    repos = Repository.where(:access_token => shas).pluck(:id, :name)
+    chain_start = Commit.where(:repository_id => repos.map(&:first))
     render json: {
       pending:  chain_start.pending.count,
       rejected: chain_start.rejected.count,
-      pending_per_project: per_repo_count(chain_start.pending),
-      rejected_per_project: per_repo_count(chain_start.rejected)
+      pending_per_project: per_repo_count(chain_start.pending, repos),
+      rejected_per_project: per_repo_count(chain_start.rejected, repos)
     }
   end
 
   private
-    def per_repo_count query
-      counts = query.count(group: :repository_id)
-      repos = Repository.find(counts.keys).inject({}) do |memo,e|
-        memo.tap{ |m| m[e.id] = e }
-      end
-      Hash[counts.to_a.map do |r_id, count|
-        [repos[r_id], count]
+    def per_repo_count query, repos
+      counts = Hash[query.count(group: :repository_id)]
+      Hash[repos.map do |r_id, name|
+        [name, counts[r_id].to_i]
       end]
     end
 end
