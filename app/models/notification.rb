54acf2d8d620b329de0e6979f5b994ae83cddf13
Michał Szajbe <michal.szajbe@gmail.com>
2013-10-16 13:13:18 +0200

    Concatenate message string with <<

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -28,7 +28,7 @@ class Notification
 
       unless hipchat_username.blank?
         message  = "@#{hipchat_username} your commit has been rejected by #{hipchat_reviwer}: "
-        message += "https://github.com/#{commit.repository.to_s}/commit/#{commit.sha}"
+        message << "https://github.com/#{commit.repository.to_s}/commit/#{commit.sha}"
         hipchat_api.rooms_message(Figaro.env.hipchat_room, "GHCR", message, 1, 'red', 'text')
       end
     end
