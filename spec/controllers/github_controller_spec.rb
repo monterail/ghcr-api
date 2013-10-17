93cdcb9b03adfaf7aee1bae05d05c8e5c6835438
Michał Szajbe <michal.szajbe@gmail.com>
2013-10-17 07:41:22 +0200

    Fixture loading helper method

--- spec/controllers/github_controller_spec.rb
+++ spec/controllers/github_controller_spec.rb
@@ -3,15 +3,14 @@ require 'spec_helper'
 describe GithubController do
   describe 'POST payload' do
     it 'should add new commits' do
-      payload = File.read(
-        File.join(File.dirname(__FILE__), '../fixtures/test_payload.json'))
+      payload = fixture('test_payload.json')
 
       expect {
         post :payload, payload: payload, format: :json
       }.to change { Commit.count }.by(3)
 
       Commit.all.all? { |c| c.status.should == 'pending' }
-      
+
       expect(response.status).to eq(200)
     end
   end
