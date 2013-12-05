e94e80bd3af9939e9855624f320e5d3cea82f8db
Dariusz Gertych <dariusz.gertych@gmail.com>
2013-12-05 11:37:57 +0100

    Add method to cleanup removed commits

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -4,6 +4,13 @@ class Repository < ActiveRecord::Base
 
   uniquify :access_token, :length => 10
 
+  def cleanup_deleted_commits
+    commits.where(status: ['pending', 'rejected']).find_each do |commit|
+      commit_url = "https://github.com/#{self.to_s}/commit/#{commit.sha}"
+      commit.destroy if Faraday.get(commit_url).status.to_i == 404
+    end
+  end
+
   def next_pending sha = nil
     query = commits.pending.order("commited_at ASC")
     if sha && commit = Commit.find_by_sha(sha)
