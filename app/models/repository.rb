4e4f790ad0fac195db8afeb7b9ed4d3cea089369
Dariusz Gertych <dariusz.gertych@gmail.com>
2013-12-18 11:10:03 +0100

    Add with_access_by scope for Repository

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -4,6 +4,20 @@ class Repository < ActiveRecord::Base
 
   uniquify :access_token, :length => 10
 
+  class << self
+    def with_access_by(username, access_type = :admin)
+      user = User.find_by!(username: username)
+      Repository.all.select do |repo|
+        access = begin
+          user.permissions(repo.to_s).try(access_type)
+        rescue Octokit::NotFound => ex
+          false
+        end
+        !!access
+      end
+    end
+  end
+
   def next_pending sha = nil
     query = commits.pending.order("commited_at ASC")
     if sha && commit = Commit.find_by_sha(sha)
