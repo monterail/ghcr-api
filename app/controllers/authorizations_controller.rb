7ccb5b7b38ebecd928593d112f30ac78b37c1c1b
Tymon Tobolski <i@teamon.eu>
2014-01-20 18:05:56 +0100

    Fix authorization
    @chytreg - plz test code before deploying :)

--- app/controllers/authorizations_controller.rb
+++ app/controllers/authorizations_controller.rb
@@ -5,7 +5,7 @@ class AuthorizationsController < ApplicationController
       render(status: :unprocessable_entity, json: { error: 'No auth info provided' }) and return
     end
     if redirect_uri = request.env['omniauth.params']['redirect_uri'] || request.env['omniauth.origin']
-      user = User.find_or_create_by(:username, auth_hash['info']['nickname']) do |u|
+      user = User.find_or_create_by(username: auth_hash['info']['nickname']) do |u|
         u.name  = auth_hash['info']['name']
         u.email = auth_hash['info']['email']
         u.github_access_token = auth_hash['credentials']['token']
@@ -13,7 +13,7 @@ class AuthorizationsController < ApplicationController
       end
 
       redirect_to URI(redirect_uri).tap { |url|
-        url.fragment = 'access_token=#{user.access_token.token}&token_type=bearer'
+        url.fragment = "access_token=#{user.access_token.token}&token_type=bearer"
       }.to_s
     else
       render status: :unprocessable_entity, json: { error: 'No redirect_uri provided' }
