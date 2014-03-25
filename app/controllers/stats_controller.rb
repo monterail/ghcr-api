dfa42b7fd701bb42ee9c6d059889bf6af26de156
Dariusz Gertych <dariusz.gertych@gmail.com>
2014-03-25 23:13:25 +0100

    More stats

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -10,28 +10,47 @@ class StatsController < ApplicationController
     render json: {
       pending:  chain_start.pending.count,
       discuss: chain_start.discuss.count,
-      pending_per_project: per_repo_count(chain_start.pending, repos),
-      discuss_per_project: per_repo_count(chain_start.discuss, repos),
+      pending_per_project: per_repo_count(chain_start.pending),
+      discuss_per_project: per_repo_count(chain_start.discuss),
       pending_per_author: per_author_count(chain_start.pending),
       discuss_per_author: per_author_count(chain_start.discuss),
-      pending_overall: overall(chain_start.pending, repos),
-      discuss_overall: overall(chain_start.discuss, repos)
+      pending_overall_per_project: overall_per_repo(chain_start.pending),
+      discuss_overall_per_project: overall_per_repo(chain_start.discuss),
+      pending_overall_per_author: overall_per_author(chain_start.pending),
+      discuss_overall_per_author: overall_per_author(chain_start.discuss),
     }
   end
 
   private
-    def overall(query, repos)
+    def overall_per_repo(query)
       counts = Hash[query.count(group: :repository_id)]
-      result = repos.map do |r_id, name|
-        [name, per_author_count(query.where(repository_id: r_id))] if counts[r_id]
+      result = counts.map do |r_id, count|
+        [
+          Repository.find(r_id).full_name,
+          per_author_count(query.where(repository_id: r_id))
+        ]
       end.compact.sort {|a, b| b[1].values.sum <=> a[1].values.sum }
       Hash[result]
     end
 
-    def per_repo_count(query, repos)
+    def overall_per_author(query)
+      counts = Hash[query.count(group: :author_id)]
+      result = counts.map do |author_id, count|
+        [
+          User.find(author_id).name,
+          per_repo_count(query.where(author_id: author_id))
+        ]
+      end.compact.sort {|a, b| b[1].values.sum <=> a[1].values.sum }
+      Hash[result]
+    end
+
+    def per_repo_count(query)
       counts = Hash[query.count(group: :repository_id)]
-      result = repos.map do |r_id, name|
-        [name, counts[r_id].to_i] if counts[r_id]
+      result = counts.map do |r_id, count|
+        [
+          Repository.find(r_id).full_name,
+          count.to_i
+        ]
       end.compact.sort {|a, b| b[1] <=> a[1] }
       Hash[result]
     end
@@ -39,7 +58,10 @@ class StatsController < ApplicationController
     def per_author_count(query)
       counts = Hash[query.count(group: :author_id)]
       result = counts.map do |author_id, count|
-        [User.find(author_id).name, count.to_i]
+        [
+          User.find(author_id).name,
+          count.to_i
+        ]
       end.compact.sort {|a, b| b[1] <=> a[1] }
       Hash[result]
     end
