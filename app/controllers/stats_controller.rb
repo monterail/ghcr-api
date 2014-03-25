eef7de5295c2c4f980761ad0f2e558c3296c6333
Dariusz Gertych <dariusz.gertych@gmail.com>
2014-03-25 18:27:34 +0100

    Add discuss and pending overall

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -13,11 +13,21 @@ class StatsController < ApplicationController
       pending_per_project: per_repo_count(chain_start.pending, repos),
       discuss_per_project: per_repo_count(chain_start.discuss, repos),
       pending_per_author: per_author_count(chain_start.pending),
-      discuss_per_author: per_author_count(chain_start.discuss)
+      discuss_per_author: per_author_count(chain_start.discuss),
+      pending_overall: overall(chain_start.pending, repos),
+      discuss_overall: overall(chain_start.discuss, repos)
     }
   end
 
   private
+    def overall(query, repos)
+      counts = Hash[query.count(group: :repository_id)]
+      result = repos.map do |r_id, name|
+        [name, per_author_count(query.where(repository_id: r_id))] if counts[r_id]
+      end.compact.sort {|a, b| b[1].values.sum <=> a[1].values.sum }
+      Hash[result]
+    end
+
     def per_repo_count(query, repos)
       counts = Hash[query.count(group: :repository_id)]
       result = repos.map do |r_id, name|
